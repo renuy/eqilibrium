3bd0ec5f20d121895cda472e3fbdc1a4d6a28b91
Renu Yarday <Renu.Yarday@emc.com>
2018-03-21 15:13:09 +0530

    WIP

--- equilibrium.rb
+++ equilibrium.rb
@@ -1,6 +1,20 @@
 class Equilibrium
-  def process(array)
-    array
+  def process(ip_arr)
+    eq_arr=[]
+    puts ip_arr.length
+    arr_last_index = ip_arr.length - 1
+    (0..arr_last_index).each do |i|
+      arr1 = ip_arr[0,i]
+      arr2 = ip_arr[i+1,arr_last_index]
+      puts "#{arr1}"
+      puts "#{arr2}"
+
+      puts "#{arr1.sum}==#{arr2.sum}"
+      if arr1.sum == arr2.sum then
+        eq_arr.push(i)
+      end
+    end
+    eq_arr
   end
 
 end
--- equilibrium_test.rb
+++ equilibrium_test.rb
@@ -4,8 +4,21 @@ describe Equilibrium do
     input_arr = [-7, 1, 5, 2, -4, 3, 0]
     eq = Equilibrium.new
     op = eq.process(input_arr)
-    op.should equal([3, 6])
+    op.should eql([3, 6])
   end
+  it "Array of lenght 1 is always in equilibrium" do
+    input_arr = [7]
+    eq = Equilibrium.new
+    op = eq.process(input_arr)
+    op.should eql([0])
+  end
+  it "Empty Array, for empty input " do
+    input_arr = []
+    eq = Equilibrium.new
+    op = eq.process(input_arr)
+    op.should eql([])
+  end
+
 end
 
 
